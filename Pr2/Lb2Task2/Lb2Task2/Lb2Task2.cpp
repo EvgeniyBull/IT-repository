3469cc04803c417fb6e9fa410b444dbde3328da5
EvgeniyBull <zhekatheofficial@gmail.com>
2024-10-05 12:16:49 +0300

    correct task 2

--- Pr2/Lb2Task2/Lb2Task2/Lb2Task2.cpp
+++ Pr2/Lb2Task2/Lb2Task2/Lb2Task2.cpp
@@ -10,33 +10,33 @@ int main() {
 	cin >> firstyear;
 	cout << "Prinr second year: ";
 	cin >> secondyear;
-	while (true) {
+
 		
 		if (firstyear > 0 && secondyear > 0 && secondyear > firstyear) {
-			break;
+			difference = secondyear - firstyear;
+			cout << "First year: " << firstyear << endl;
+			cout << "Second year: " << secondyear << endl;
+			cout << "Number of years in between: " << difference << endl;
+
+			if (difference > 100) {
+				cout << "The program exits. The number of years is more than 100" << endl;
+				return 0;
+			}
+
+			for (int i = 1; i <= difference; i++) {
+				if (i % 10 == 0) {
+					every10++;
+				}
+			}
+
+			cout << "Number of years that are multiples of 10: " << every10 << endl;
 		}
 		else {
-			cout << "Error" << endl;
+			cout << "Error, false value" << endl;
 		}
 
-		difference = secondyear - firstyear;
-		cout << "First year: " << firstyear << endl;
-		cout << "Second year: " << secondyear << endl;
-		cout << "Number of years in between: " << difference << endl;
-
-		if (difference > 100) {
-			cout << "The program exits. The number of years is more than 100" << endl;
-			return 0;
-		}
-
-		for (int i = 1; i <= difference; i++) {
-			if (i % 10 == 0) {
-				every10++;
-			}
-		}
-
-		cout << "Number of years that are multiples of 10: " << every10 << endl;
+		
 
 		return 0;
-	}
+
 }
